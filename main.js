29b6b6c10f1ff7cc85e6dabae6137da498979145
akash1297 <akash.zerox505@gmail.com>
2020-12-04 14:05:23 +0530

    add changes

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const testarea = document.querySelector(".test-area");
 var timer = [0, 0, 0, 0];
 var interval;
 var timer_reset = false;
+var count = 0;
 
 
 function leadingZero(time) {
@@ -14,15 +15,16 @@ function leadingZero(time) {
     }
     return time;
 }
+
 function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     time_.innerHTML = currentTime;
     timer[3]++;
-
     timer[0] = Math.floor((timer[3] / 100) / 60);
     timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
+
 function spellcheck() {
     let textentered = textarea_.value;
     let origintextMatch = textgiven.substring(0, textentered.length);
@@ -36,34 +38,26 @@ function spellcheck() {
             text_enter.style.borderColor = "#65ccf3";
         } else {
             text_enter.style.borderColor = "#E95D0F";
-
-
         }
     }
-
 }
 
 function start_calculating() {
     let textenteredlength = textarea_.value.length;
-
     if (textenteredlength === 0 && !timer_reset) {
         timer_reset = true;
         interval = setInterval(runTimer, 10);
     }
 
 }
-function reset_b() {
 
+function reset_b() {
     clearInterval(interval);
     interval = null;
     timer = [0, 0, 0, 0];
-
     timer_reset = false;
-
-
 }
 
 textarea_.addEventListener("keypress", start_calculating, false);
 textarea_.addEventListener("keyup", spellcheck, false);
 reset_button.addEventListener("click", reset_b, false);
-
